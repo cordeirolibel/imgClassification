f305ae98dca68e0241ea993eee8550eae88a6ecd
cordeirolibel <gustavolibel@gmail.com>
2017-05-27 13:50:03 +0000

    sync

--- main.py
+++ main.py
@@ -16,40 +16,40 @@ from camera import *
 
 inv = False
 
-k=0
+k=1
 
 if runOnRasp():
     camera.resolution = (1296, 972)
 
-#for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-while True :
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#while True :
 	print('frame: '+str(k))
-	k+=1
+	
 
 	if runOnRasp():
 		image = frame.array
 	else:
 		image = capture()
 
-	show(image,"Original", size = 300)
-	key1 = cv2.waitKey(10) & 0xFF
+	#show(image,"Original", size = 300)
+	#key1 = cv2.waitKey(10) & 0xFF
 
-	image = cutBorder(image, inv = inv)
+	#image = cutBorder(image, inv = inv)
 	#show(image, 'Without Border')
 
 	#Contours - identify Objects
-	objs_yes,objs_not = identifyObjects(image)
+	#objs_yes,objs_not = identifyObjects(image)
 
 	#Calculate attributes for the Deep Learning
-	attributes(image,objs_yes)
+	#attributes(image,objs_yes)
 
 	#Draw the contours and the center of mass
-	image = drawCnts(image,objs_yes,objs_not, attributes = True)
+	#image = drawCnts(image,objs_yes,objs_not, attributes = True)
 
 	show(image,"Final")
 	#cv2.imshow("Video", image)
 	key2 = cv2.waitKey(10) & 0xFF
-	
+	key1 = key2
     # clear the stream in preparation for the next frame
 	if runOnRasp():
 		rawCapture.truncate(0)
@@ -57,10 +57,14 @@ while True :
     # if the `q` key was pressed, break from the loop
 	if (key1 == ord("q")) or (key2 == ord("q") ):
 		break
+	if (key1 == ord("g")) or (key2 == ord("g") ):
+		cv2.imwrite("imgs/img"+str(k)+".jpg",image)
+		k+=1
 		
+	
 	#save image
 	#cv2.imwrite('imgs/teste.jpg',image)
-	break
+	#break
 
 cv2.waitKey(0)
 
